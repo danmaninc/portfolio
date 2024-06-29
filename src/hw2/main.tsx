4a233eca88d8f29f49ca8f0214d105cab497683b
Dan <dan@dminc.ru>
2024-06-29 18:02:46 +0300

    chore: move css of hw2 to module

--- src/hw2/main.tsx
+++ src/hw2/main.tsx
@@ -1,5 +1,5 @@
 import {Section} from "../components/layout/section/Section.tsx";
-import './main.css'
+import './main.module.css'
 import {ComicManager} from "../components/hw2/ComicManager.tsx";
 export function Homework2() {
     return (
