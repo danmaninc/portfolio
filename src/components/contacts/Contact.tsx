f0b7c360709b8c570584a10fee8c1eb1636a7db6
Dan <dan@dminc.ru>
2024-06-29 16:32:04 +0300

    feat: create contacts container

---
+++ src/components/contacts/Contact.tsx
@@ -0,0 +1,9 @@
+interface ContactProps {
+    id: string;
+    link: string;
+    icon: string;
+}
+export function Contact({ id, link, icon }: ContactProps) {
+    return <a className="contact" id={id} href={link}><i
+        className={icon}></i></a>
+}
