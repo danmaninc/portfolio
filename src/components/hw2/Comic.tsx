bfdca82068d78b33fadee8a53b32cc3c4ddcc5fd
Dan <dan@dminc.ru>
2024-06-29 17:45:26 +0300

    chore: create Comic view

---
+++ src/components/hw2/Comic.tsx
@@ -0,0 +1,15 @@
+interface ComicProps {
+    comicLink: string;
+    comicAlt: string;
+    title: string;
+    date: string;
+}
+export function Comic({ comicLink, comicAlt, title, date } : ComicProps) {
+    return (
+        <div>
+            <h1>{title}</h1>
+            <p>{date}</p>
+            <img src={comicLink} alt={comicAlt}/>
+        </div>
+    )
+}
