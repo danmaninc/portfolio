a7d31b78a6a38b9192a68243b679a0e938e0390c
Dan <dan@dminc.ru>
2024-06-30 00:14:25 +0300

    chore: change to hash

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import {Homework2} from "./hw2/main.tsx";
 // import {Route, Routes} from "react-router-dom";
 // import {Homework2} from "./hw2/main.tsx";
@@ -14,13 +14,13 @@ import {Homework2} from "./hw2/main.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
           <Routes>
               <Route path="/" element={<App/>}/>
               <Route path="/hw2" element={<Homework2/>}/>
           </Routes>
           {/*<App/>*/}
-      </BrowserRouter>
+      </HashRouter>
       {/*<RouterProvider router={router} />*/}
   </React.StrictMode>,
 )
