98dc24a97814cea4761510e7835d7809d9b1ede8
Dan <dan@dminc.ru>
2024-06-29 23:33:45 +0300

    chore: change router

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,16 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
 import {Homework2} from "./hw2/main.tsx";
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-    },
-    {
-        path: "/hw2",
-        element: <Homework2 />,
-    }
-]);
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+      <Router>
+          <Routes>
+            <Route path="/" element={<App/>}/>
+            <Route path="hw2/" element={<Homework2/>}/>
+          </Routes>
+      </Router>
+      {/*<RouterProvider router={router} />*/}
   </React.StrictMode>,
 )
