4296cea6eccf1649415927e7480c7bd0ba57c9a1
Dan <dan@dminc.ru>
2024-06-29 16:32:20 +0300

    chore: configure router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
-
+import {Homework2} from "./hw2/main.tsx";
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/hw2",
+                element: <Homework2 />,
+            }
+        ],
+    },
+]);
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
