0c1321cf693b37af03ff476050782df7e8734d47
Dan <dan@dminc.ru>
2024-06-30 00:10:47 +0300

    chore: try to change router

--- src/App.tsx
+++ src/App.tsx
@@ -11,40 +11,40 @@ function App() {
 
   return (
     <>
-        <Section id={"introduction"}>
-            <Profile
-                name="Daniil Nikulin"
-                nicknames={["danmaninc"]}
-                description={<ProfileDescription/>}
-                avatarLink={"https://lh3.googleusercontent.com/a/ACg8ocKkp2cxsX99Aa_fy1KVXMueG9z-uhD593aNJ3F3qHFNM82Ul55v=s96-c-rg-br25"}
-                avatarAlt={"Daniil's avatar"}
-            />
-        </Section>
-        <Section id={"contacts"} heading="Contact me" centered={true} removeMargin={true}>
-            <ContactsContainer>
-                <Contact id="gh-link" link="https://github.com/danmaninc/" icon="github"/>
-                <Contact id="tg-link" link="https://t.me/JustDanman" icon="telegram"/>
-                <Contact id="email" link="mailto:dminc.work@gmail.com" icon="envelope"/>
-            </ContactsContainer>
-        </Section>
-        <Section id={"projects"} heading="Projects">
-            <ProjectContainer>
-                <Project
-                    title={"Stress Testers app"}
-                    description={"Web application for code stress-testing"}
-                    projectLink={"https://stress.dminc.ru/"}
-                    logoLink={"https://sun1-19.userapi.com/s/v1/if2/8DJUQPEL7edfo8Zy6_o3dmTqFNhIq7m4cWI7n2H_1UMTtRfeEhJjszkkgep8qQk4D1SvWJKfxUcjKmzn-v3SLMo7.jpg?size=666x676&quality=96&crop=107,149,666,677&ava=1"}
-                    logoAlt={"Stress Testers logo"}
+            <Section id={"introduction"}>
+                <Profile
+                    name="Daniil Nikulin"
+                    nicknames={["danmaninc"]}
+                    description={<ProfileDescription/>}
+                    avatarLink={"https://lh3.googleusercontent.com/a/ACg8ocKkp2cxsX99Aa_fy1KVXMueG9z-uhD593aNJ3F3qHFNM82Ul55v=s96-c-rg-br25"}
+                    avatarAlt={"Daniil's avatar"}
                 />
-                <Project
-                    title={"InNoHassle"}
-                    description={"Web services for Innopolis students"}
-                    projectLink={"https://innohassle.ru/"}
-                    logoLink={"https://innohassle.ru/_next/static/media/logo.1d464cf5.svg"}
-                    logoAlt={"InNoHassle logo"}
-                />
-            </ProjectContainer>
-        </Section>
+            </Section>
+            <Section id={"contacts"} heading="Contact me" centered={true} removeMargin={true}>
+                <ContactsContainer>
+                    <Contact id="gh-link" link="https://github.com/danmaninc/" icon="github"/>
+                    <Contact id="tg-link" link="https://t.me/JustDanman" icon="telegram"/>
+                    <Contact id="email" link="mailto:dminc.work@gmail.com" icon="envelope"/>
+                </ContactsContainer>
+            </Section>
+            <Section id={"projects"} heading="Projects">
+                <ProjectContainer>
+                    <Project
+                        title={"Stress Testers app"}
+                        description={"Web application for code stress-testing"}
+                        projectLink={"https://stress.dminc.ru/"}
+                        logoLink={"https://sun1-19.userapi.com/s/v1/if2/8DJUQPEL7edfo8Zy6_o3dmTqFNhIq7m4cWI7n2H_1UMTtRfeEhJjszkkgep8qQk4D1SvWJKfxUcjKmzn-v3SLMo7.jpg?size=666x676&quality=96&crop=107,149,666,677&ava=1"}
+                        logoAlt={"Stress Testers logo"}
+                    />
+                    <Project
+                        title={"InNoHassle"}
+                        description={"Web services for Innopolis students"}
+                        projectLink={"https://innohassle.ru/"}
+                        logoLink={"https://innohassle.ru/_next/static/media/logo.1d464cf5.svg"}
+                        logoAlt={"InNoHassle logo"}
+                    />
+                </ProjectContainer>
+            </Section>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Homework2} from "./hw2/main.tsx";
+// import {Route, Routes} from "react-router-dom";
+// import {Homework2} from "./hw2/main.tsx";
+// import {Homework2} from "./hw2/main.tsx";
 
-const router = createBrowserRouter(createRoutesFromElements([
-    <Route path="/" element={<App/>}/>,
-    <Route path="hw2/" element={<Homework2/>}/>]
-), { basename: '/portfolio'})
+// const router = createBrowserRouter(createRoutesFromElements([
+//     <Route path="/" element={<App/>}/>,
+//     <Route path="hw2/" element={<Homework2/>}/>]
+// ), { basename: '/portfolio'})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<App/>}/>
+              <Route path="/hw2" element={<Homework2/>}/>
+          </Routes>
+          {/*<App/>*/}
+      </BrowserRouter>
+      {/*<RouterProvider router={router} />*/}
   </React.StrictMode>,
 )
