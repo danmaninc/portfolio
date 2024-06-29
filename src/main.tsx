1b44d1d62a91e92e5fb539e2d06a9704e52662b6
Dan <dan@dminc.ru>
2024-06-29 23:51:56 +0300

    chore: add basename

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import {HashRouter as Router, Route, Routes} from "react-router-dom";
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
 import {Homework2} from "./hw2/main.tsx";
+
+const router = createBrowserRouter(createRoutesFromElements([
+    <Route path="/" element={<App/>}/>,
+    <Route path="hw2/" element={<Homework2/>}/>]
+), { basename: '/portfolio'})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <Router>
-          <Routes>
-            <Route path="/" element={<App/>}/>
-            <Route path="hw2/" element={<Homework2/>}/>
-          </Routes>
-      </Router>
-      {/*<RouterProvider router={router} />*/}
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
