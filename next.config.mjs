1a397054731898defe0f8d859c859c41e42e2046
Dan <whatdanman@gmail.com>
2024-07-06 22:06:55 +0300

    chore: prepare to gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "export",
+    basePath: "/portfolio",
     images: {
+        unoptimized: true,
         remotePatterns: [
             {
                 protocol: 'https',
