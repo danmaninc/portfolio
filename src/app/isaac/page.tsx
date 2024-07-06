b25b51954c1e267943b3f8dfe25f5a71687ceb8f
Dan <whatdanman@gmail.com>
2024-07-06 22:03:41 +0300

    chore: infinite loop

--- src/app/isaac/page.tsx
+++ src/app/isaac/page.tsx
@@ -4,7 +4,7 @@ export default function Isaac() {
     return (
         <Section id={"isaac"} centered={true}>
             <iframe width="560" height="315"
-                    src="https://www.youtube.com/embed/cCe7qPu5dlI?autoplay=1&loop=1&amp;controls=0"
+                    src="https://www.youtube.com/embed/cCe7qPu5dlI?autoplay=1&loop=1&autopause=0&amp;controls=0"
                     title="YouTube video player"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                     className="rounded-xl"
