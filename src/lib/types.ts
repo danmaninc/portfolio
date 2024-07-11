ddb23010a84f35f61b865fc344ef0f4a1d2bbf3a
Dan <whatdanman@gmail.com>
2024-07-11 21:28:32 +0300

    fix: add types

---
+++ src/lib/types.ts
@@ -0,0 +1,15 @@
+export interface IComic {
+    month: string;
+    num: number;
+    link: string;
+    year: string;
+    news: string;
+    safe_title: string;
+    transcript: string;
+    alt: string;
+    img: string;
+    title: string;
+    day: string;
+    dateFormatted?: string;
+    dateFromNow?: string;
+}
