b7b526cc2679300b85e5f851d617994a6e962cee
Dan <whatdanman@gmail.com>
2024-07-11 22:34:01 +0300

    chore: test husky

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-        <main className="w-auto my-10 mb-0 mx-[25rem] min-[374px]:max-[960px]:my-[2.5rem] min-[374px]:max-[960px]:mx-2 min-[768px]:max-[1600px]:my-10 min-[768px]:max-[1600px]:mx-24">
+    <main className="w-auto my-10 mb-0 mx-[25rem] min-[374px]:max-[960px]:my-[2.5rem] min-[374px]:max-[960px]:mx-2 min-[768px]:max-[1600px]:my-10 min-[768px]:max-[1600px]:mx-24">
       <div className="flex flex-col justify-center items-center gap-8 h-96">
         <div className="flex flex-col justify-center items-center gap-2">
           <h1 className="text-7xl font-bold">404</h1>
